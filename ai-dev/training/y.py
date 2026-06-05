6088e4d9e466a01362396c2a09289727eef29ddc
Argyanto Daiswara <argyantodaiswara@gmail.com>
2026-06-05 15:43:48 +0700

    adjusting the insight js

--- ai-dev/training/y.py
+++
@@ -1,9 +0,0 @@
-from tensorflow.keras.models import load_model
-
-model = load_model(
-    "../models/model_long_term.keras",
-    compile=False
-)
-
-print("INPUT :", model.input_shape)
-print("OUTPUT:", model.output_shape)
